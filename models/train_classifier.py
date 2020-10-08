ab78a80f79e2d5548976174e07a55894d4f0b4ab
GERMAN PINZON <germanpinzon@iMac-de-GERMAN.local>
2020-10-07 19:20:15 -0500

    Updated docstrings

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -32,21 +32,41 @@ import pickle
 
 
 def load_data(database_filepath):
+    """
+    Loads data table from the database, drops unnecessary columns, gets the category labels 
+    and splits the data in messages/target arrays.
+        
+    Params
+    --------
+        database_filepath (str): 
+            database path, created in the process_data.py script.
+                                            
+    Returns
+    --------
+        X (Pandas Series): 
+            Series with the messages translated to english.
+            
+        Y (Pandas DataFrame): 
+            DataFrame with the sparsed categories classification target data.    
+
+        category_names (list): 
+            List with the 36 category labels the model will classify the messages into.  
+    """      
     # load data from database
     database_filename_ext = 'sqlite:///' + database_filepath
     engine = create_engine(database_filename_ext)
     df = pd.read_sql_table('disaster_response_data', engine)
 
-    # I am not gone to use the original messages, so let´s drop the 'original' column:
+    # I am not going to use the original messages, so let´s drop the 'original' column:
     df.drop(['original'], axis=1, inplace = True)
     # df.isnull().sum()
 
     # There is 138 messages that have no classification. Let's hold these as a test set for later prediction (df_test), 
-    # and split the messages that do have classification into train/test (validation) sets for model training (df_train).
+    # and split the messages that do have classification into features/target (validation) dataframes for model training                (df_train).
     df_train = df[~(df.isnull().any(axis=1))]
     df_test = df[df.isnull().any(axis=1)]
     
-    # Train and test sets
+    # Messages and target dataframes
     X = df_train.iloc[:,1]
     Y = df_train.iloc[:, 3:]
     #X.head()
@@ -59,6 +79,19 @@ def load_data(database_filepath):
 
 
 def tokenize(text):
+    """
+    Tokenizer, punctuation/numbers filter and lemmatizer function for texts in English language.
+        
+    Params
+    --------
+        text (str): 
+            String to be tokenized.
+                                            
+    Returns
+    --------
+        tokens (list): 
+            List of the clean tokens found in text.  
+    """    
     stop_words = stopwords.words("english")
     lemmatizer = WordNetLemmatizer()    
     text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())    
@@ -69,6 +102,18 @@ def tokenize(text):
 
 
 def build_model():
+    """
+    MultiOutput Text Classifier model building with pipeline technique and grid search over multiple parameters.
+        
+    Params
+    --------
+    
+                                            
+    Returns
+    --------
+        cv (GridSearchCV object): 
+            Grid search pipeline MultiOutput Text Classifier model to be trained.  
+    """
     # Pipeline:
     pipeline = Pipeline([
             ('vectorizer', CountVectorizer(tokenizer=tokenize, max_df=0.5, max_features=10000)),
@@ -76,7 +121,7 @@ def build_model():
             ('clf', MultiOutputClassifier(KNeighborsClassifier(), n_jobs = -1))
         ])
 
-    # Parameters for gridsearching (Reduced version for speed purposes)
+    # Parameters for grid searching (Reduced version for speed purposes, see README)
     parameters = parameters = {
             #'vectorizer__ngram_range': ((1, 1), (1, 2)),
             #'vectorizer__max_df': (0.5, 0.75, 1.0),
@@ -92,11 +137,34 @@ def build_model():
     return cv
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    """
+    Evaluates the predictions made by the model builded in build_model() 
+    function over the test set with three metrics per category: F1_score, 
+    precision and recall and prints the results.
+        
+    Params
+    --------
+        model (sklearn model): 
+            Model to be evaluated.
+            
+        X_test (array): 
+            Array with the messages test set.            
+
+        Y_test (Pandas DataFrame): 
+            Array with the target test set.
+            
+        category_names (list): 
+            Target labels provided by load_data() function.            
+            
+    Returns
+    --------
+
+    """ 
     # Predict over validation set:
     y_pred = model.predict(X_test)
     
     # Evaluate the performance: 
-    y_test_arr = np.array(Y_test)
+    y_test_arr = np.array(Y_test) # Turn y_test to a numpy array
     for i in range(y_test_arr.shape[1]):
         y_true = np.vstack(y_test_arr[:,i])
         y_predictions = np.vstack(y_pred[:,i]) 
@@ -105,11 +173,29 @@ def evaluate_model(model, X_test, Y_test, category_names):
         recall = classification_report_str[-25:-20]
         f1 = classification_report_str[-15:-10]
         print(category_names[i] + ':')
-        print('F1_score: {}'.format(f1), '   % Precision: {}'.format(precision), '   % Recall: {}'.format(recall))
+        print('F1_score: {}'.format(f1), 
+              '   % Precision: {}'.format(precision),
+              '   % Recall: {}'.format(recall)
+             )
         print('\n')
 
 
 def save_model(model, model_filepath):
+    """
+    Serializes and saves the model builded.
+        
+    Params
+    --------
+        model (sklearn model): 
+            Model to be serialized and saved.
+            
+        model_filepath (str): 
+            File path of the pickled model.            
+
+    Returns
+    --------
+
+    """ 
     filename = model_filepath
     pickle.dump(model, open(filename, 'wb'))
 
