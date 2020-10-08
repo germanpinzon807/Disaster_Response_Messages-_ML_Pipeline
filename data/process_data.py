35471282f43439fed2fba58150a7895005c2ee2d
GERMAN PINZON <germanpinzon@iMac-de-GERMAN.local>
2020-10-07 19:19:10 -0500

    Updated docstrings

--- data/process_data.py
+++ data/process_data.py
@@ -1,43 +1,53 @@
- # Import libraries
+# Import modules:
 import sys
 import pandas as pd
 from sqlalchemy import create_engine    
 
 
 def load_data(messages_filepath, categories_filepath):
-    
-    #1. Load datasets
-    # load messages dataset
+    """
+    Loads datasets, merges them in a single dataframe and transforms 
+    the categories data in a suited form for the ML model training.
+        
+    Params
+    --------
+        messages_filepath (str): 
+            Messages original dataset path.
+
+        categories_filepath (str):
+            Categories original dataset path.
+                                            
+    Returns
+    --------
+        df (Pandas DataFrame): 
+            Dataframe with the original messages and a sparse structure of 0's and 1's encoded classification per category.  
+    """    
+    # Step 1. Load datasets:
+        # load messages dataset
     messages = pd.read_csv(messages_filepath)
-    #messages.head()
     
-    # load categories dataset
+        # load categories dataset
     categories = pd.read_csv(categories_filepath)
-    #categories.head()
     
-    # 2. Merge datasets
-    # merge datasets
+    # Step 2. Merge datasets:
+        # merge datasets
     df = messages.merge(categories, left_on='id', right_on='id')
-    #df.head()
     
-    # 3. Split categories into separate category columns:
-    # create a dataframe of the 36 individual category columns
+    # Step 3. Split categories into separate category columns:
+        # create a dataframe of the 36 individual category columns
     categories = categories.categories.str.split(';',expand=True)
-    #categories.head()
     
-    # select the first row of the categories dataframe
+        # select the first row of the categories dataframe
     row = categories.iloc[0]
     row[32][:-2]
 
-    # Extract from this row a list of new column names for categories.
+        # Extract from this row a list of new column names for categories.
     category_colnames =list(row.apply(lambda x: x[:-2]))
-    #print(category_colnames)
     
-    # rename the columns of `categories`
+        # rename the columns of `categories`
     categories.columns = category_colnames
-    #categories.head()
     
-    # 4. Convert category values to just numbers 0 or 1
+    # Step 4. Convert category values to just numbers 0 or 1:
     for column in categories:
         # set each value to be the last character of the string
         categories[column] = categories[column].apply(lambda x: x[-1])
@@ -46,32 +56,59 @@ def load_data(messages_filepath, categories_filepath):
         categories[column] = categories[column].astype('int')    
     #categories.info()
     
-    # 5. Replace categories column in df with new category columns
-    # drop the original categories column from `df`
+    # Step 5. Replace categories column in df with new category columns:
+        # drop the original categories column from `df`
     df.drop(['categories'], axis=1, inplace=True)
     #df.head()
     
-    # concatenate the original dataframe with the new `categories` dataframe
+        # concatenate the original dataframe with the new `categories` dataframe
     df = pd.concat([df, categories], axis=1)
-    #df.head()
         
     return df
 
 def clean_data(df):
-    # 6. Remove duplicates
-    # check number of duplicates
+    """
+    Check and drop any duplicates that may be present in the dataframe provided by load_data() function.
+        
+    Params
+    --------
+        df (Pandas DataFrame): 
+            Dataframe provided by load_data() function.
+                                            
+    Returns
+    --------
+        df (Pandas DataFrame): 
+            Dataframe provided by load_data() function with no duplicates.  
+    """    
+    # Step 6. Remove duplicates
+        # check number of duplicates
     #df.duplicated(keep='last').sum()
     
-    # drop duplicates
+        # drop duplicates
     df.drop_duplicates(keep='last', inplace=True)
     
-    # check number of duplicates after the drop
+        # check number of duplicates after the drop
     #df.duplicated(keep='last').sum()
     
     return df   
 
 
 def save_data(df, database_filename):
+    """
+    Database setup and table creation with the dataframe provided by clean_data() function.
+        
+    Params
+    --------
+        df (Pandas DataFrame): 
+            Dataframe provided by clean_data() function.
+            
+        database_filename (str):
+            Database file path to create.                              
+    Returns
+    --------
+ 
+    """
+    # Step 7. Database setup and table creation:
     database_filename_ext = 'sqlite:///' + database_filename
     engine = create_engine(database_filename_ext)
     df.to_sql('disaster_response_data' , engine, index=False) 
